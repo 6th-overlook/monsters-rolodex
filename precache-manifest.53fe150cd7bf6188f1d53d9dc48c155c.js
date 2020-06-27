0c1fcc216be4abe926db4ace3bf30694a0b19989
6th-overlook <6th.overlook@gmail.com>
2020-06-27 02:51:53 +0000

    Updates

---
+++ precache-manifest.53fe150cd7bf6188f1d53d9dc48c155c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9423e8767337197edadc0311c91a68c5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c8b7cd493b23fb6bf367",
+    "url": "/monsters-rolodex/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "c298b14c810ea1488f93",
+    "url": "/monsters-rolodex/static/css/main.56dd5024.chunk.css"
+  },
+  {
+    "revision": "c8b7cd493b23fb6bf367",
+    "url": "/monsters-rolodex/static/js/2.526843bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/monsters-rolodex/static/js/2.526843bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c298b14c810ea1488f93",
+    "url": "/monsters-rolodex/static/js/main.59be46ae.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
